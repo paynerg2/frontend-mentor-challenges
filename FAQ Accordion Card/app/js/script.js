ff5f5e46cfd1efc290cb91ddbfff5b919575fd5f
Rodney Payne <paynerg2@gmail.com>
2021-09-29 13:36:11 -0400

    (Bug Fix) Fix inconsistency in active selection.

--- FAQ Accordion Card/app/js/script.js
+++ FAQ Accordion Card/app/js/script.js
@@ -4,30 +4,39 @@ console.log('Hello world');
 var accordions = document.getElementsByClassName('accordion');
 
 for (i = 0; i < accordions.length; i++) {
+    accordions[i].setAttribute('name', i.toString());
     accordions[i].addEventListener('click', function () {
-        /* Toggle between adding and removing the "active" class,
-      to highlight the button that controls the panel */
-        clearActiveStates();
-        this.classList.toggle('active');
-
-        /* Toggle between hiding and showing the active panel */
-        var panel = this.nextElementSibling;
-        if (panel.style.display === 'block') {
-            panel.style.display = 'none';
-        } else {
-            panel.style.display = 'block';
-        }
+        toggleActive(this);
+        clearOtherActiveStates(this.getAttribute('name'));
     });
-}
-
-function clearActiveStates() {
-    for (i = 0; i < accordions.length; i++) {
-        const acc = accordions[i];
-        acc.classList.remove('active');
-        hidePanel(acc);
-    }
 }
 
 function hidePanel(accordion) {
     accordion.nextElementSibling.style.display = 'none';
+}
+
+function toggleActive(x) {
+    /* Toggle between adding and removing the "active" class,
+      to highlight the button that controls the panel */
+    x.classList.toggle('active');
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = x.nextElementSibling;
+    if (panel.style.display === 'block') {
+        panel.style.display = 'none';
+    } else {
+        panel.style.display = 'block';
+    }
+}
+
+function clearOtherActiveStates(name) {
+    for (i = 0; i < accordions.length; i++) {
+        console.log(accordions[i].getAttribute('name'));
+        var currentElementName = accordions[i].getAttribute('name');
+        console.log(currentElementName === name);
+        if (accordions[i].getAttribute('name') !== name) {
+            accordions[i].classList.remove('active');
+            hidePanel(accordions[i]);
+        }
+    }
 }
